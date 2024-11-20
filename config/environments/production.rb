1f8dfbd622b141e8452403c0b132ce8dd17badb1
Todd Neale <robotneale@gmail.com>
2024-11-20 16:17:30 +0000

    Upgrade to Rails 7.1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  # config.force_ssl = true
 
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "Currency_Converter_production"
+  # config.active_job.queue_name_prefix = "currency_converter_production"
 
   config.action_mailer.perform_caching = false
 
